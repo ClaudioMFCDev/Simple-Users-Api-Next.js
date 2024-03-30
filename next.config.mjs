7857dcd94edc6c34919cf4ed55f2c9600d6b5332
ClaudioMFCDev <97706777+ClaudioMFCDev@users.noreply.github.com>
2024-03-30 16:12:16 -0300

    Books showed whit their covers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //add image api library
+  images: {
+    domains: ['covers.openlibrary.org']
+  }
+};
 
 export default nextConfig;
--- src/components/DashboardForm.tsx
+++ src/components/DashboardForm.tsx
@@ -1,6 +1,6 @@
+import Image from "next/image";
 
-
-export default async function DashboardPage(author : any) {
+export default async function DashboardPage(author: any) {
     const getDashboard = async () => {
         let author = 'conan'
         const response = await fetch(`https://openlibrary.org/search.json?author=${author}}`);
@@ -10,16 +10,22 @@ export default async function DashboardPage(author : any) {
     const data = await getDashboard();
 
     return (
-        
-        <div className="div">
+
+        <main>
             <div>Libros</div>
-            <div>
-                {data.map( (libro: any) => (
-                    <div key={libro.key} className="flex items-center gap-2">
-                        <span>{libro.title}</span>                      
+            <div className="flex items-center gap-y-8 gap-x-2 flex-wrap">
+                {data.map((libro: any) => (
+                    <div key={libro.key} className="flex flex-col items-center gap-2 w-52">
+                        <Image 
+                        src={`https://covers.openlibrary.org/b/id/${libro.cover_i}-M.jpg`} 
+                        alt="cover-title" 
+                        width={100} 
+                        height={100} 
+                        />
+                        <span>{libro.title}</span>
                     </div>
-                ) )}
+                ))}
             </div>
-        </div>
+        </main>
     )
 }
